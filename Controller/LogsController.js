6f322fb60f52d0706a1147a861d860d3886262bb
“juanP1996” <parrajp1996@gmail.com>
2022-08-22 16:11:30 +0200

    feat: separados losmlogs por espacios y comillas, faltan detalles como enviar los logs

--- Controller/LogsController.js
+++ Controller/LogsController.js
@@ -1,20 +1,55 @@
 const LogsController = {}
 
-const datos = require('../tete.json');
-const filename = '../logs.txt';
+// const datos = require('../tete.json');
+const filename = '../textico.txt';
+
+// const readline = require('readline');
 const fs = require('fs'),
     path = require('path'),
-    filePath = path.join(__dirname, filename);
+    // readline = require('readline');
+filePath = path.join(__dirname, filename);
 
-LogsController.getAll = (req, res) => {
+const logs = [] ; 
+
+LogsController.getAll =  (req, res) => {
 
     try {
         var data = fs.readFileSync(filePath, 'utf8');
+
+        data =  data.split(/ +(?=(?:(?:[^"]*"){2})*[^"]*$)/g);
+
+        const items = data.slice(0, 5)
+
+        logs.push(items)
+
+        console.log(logs, "ESTO ES LOGS")
+
+        
+
+        if(data.length > 0){ 
+            console.log(data);
+            res.send(data);
+        }else {
+            res.send('ERROR');
+        
+        }
+
+        // const rl = readline.createInterface({
+        //     input: fs.createReadStream(filePath),
+        //     output: process.stdout,
+        //     terminal: false
+        // })
+
+        // rl.on('line', (line) => {
+        //     console.log(line);
+        // });
+
+
+
         /*
         aqui tiene que estar el codigo para leer el archivo linea a linea 
         */
 
-        res.send(data);
 
     } catch (err) {
         console.log('err: ' + err);
